88fbbd5fa27e4cc6a3330230cbbf4aaa3db56e95
Charles Seaton <seatonct@gmail.com>
2023-12-04 14:53:57 -0600

    setup

--- dramproject/urls.py
+++ dramproject/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from dramapi.views import UserViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register', UserViewSet.as_view(
+        {'post': 'register_account'}), name='register'),
+    path('login', UserViewSet.as_view({'post': 'user_login'}), name='login'),
 ]
-
