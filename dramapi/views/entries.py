8231344381d440ecbdc3ffbf114652f9dd3cd0c4
Charles Seaton <seatonct@gmail.com>
2023-12-07 11:59:28 -0600

    added list and retrieve methods to viewset

--- dramapi/views/entries.py
+++ dramapi/views/entries.py
@@ -34,3 +34,26 @@ class EntrySerializer (serializers.ModelSerializer):
         model = Entry
         fields = ['id', 'is_owner', 'user_id', 'whiskey', 'type', 'country', 'part_of_country', 'age_in_years', 'proof', 'color',
                   'mash_bill', 'maturation_details', 'nose', 'palate', 'finish', 'rating', 'notes', 'publication_date', 'image_url', 'published']
+
+
+class EntryViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        requester_id = request.query_params.get('user_id')
+
+        if requester_id is not None:
+            entries = Entry.objects.filter("user_id" == requester_id).all()
+        else:
+            entries = Entry.objects.all()
+        serializer = EntrySerializer(
+            entries, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            entry = Entry.objects.get(pk=pk)
+            serializer = EntrySerializer(
+                entry, many=False, context={'request': request})
+            return Response(serializer.data)
+        except Entry.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
