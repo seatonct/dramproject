1bfcc9558f8f1523ea5d231fa9724c2519b2ddf8
Charles Seaton <seatonct@gmail.com>
2023-12-08 12:02:30 -0600

    updated color model, viewset, and fixtures to include 'tailwind_name'

--- dramapi/models/color.py
+++ dramapi/models/color.py
@@ -5,3 +5,4 @@ class Color(models.Model):
     label = models.CharField(max_length=300)
     color_grade = models.DecimalField(max_digits=3, decimal_places=1)
     hex_code = models.CharField(max_length=6)
+    tailwind_name = models.CharField(max_length=155)
