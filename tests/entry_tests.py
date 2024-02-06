d380cc606d6b89fb931834a5802c4cba9b89f9ea
Charles Seaton <seatonct@gmail.com>
2024-02-06 14:46:35 -0600

    added test_get_entry method to test entry_tests module

--- tests/entry_tests.py
+++ tests/entry_tests.py
@@ -74,3 +74,61 @@ class EntryTests(APITestCase):
         self.assertEqual(json_response["rating"], {
                          'id': 5, 'number_rating': 4, 'label': 'very good'})
         self.assertEqual(json_response["notes"], "tasty whiskey")
+
+    def test_get_entry(self):
+        '''Ensure we can get an existing entry'''
+
+        # Seed the database with an entry.
+        entry = Entry()
+        entry.whiskey = "Test Whiskey"
+        entry.whiskey_type = Type.objects.get(pk=7)
+        entry.country = "USA"
+        entry.part_of_country = "Tennessee"
+        entry.age_in_years = 12
+        entry.proof = 100
+        entry.color = Color.objects.get(pk=10)
+        entry.mash_bill = "100% malted barley"
+        entry.maturation_details = "former Tennessee whiskey barrels"
+        entry.nose = "roasted grain, caramel, vanilla"
+        entry.palate = "apple pie, vanilla icecream"
+        entry.finish = "long and sweet"
+        entry.rating = Rating.objects.get(pk=5)
+        entry.notes = "tasty whiskey"
+        entry.user = self.user
+
+        entry.save()
+
+        # Initiate a request and store response.
+        response = self.client.get(f"/entries/{entry.id}")
+
+        # Parse the JSON in the response body.
+        json_response = json.loads(response.content)
+
+        # Assert that the entry was retrieved.
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Assert that the values are correct.
+        self.assertEqual(json_response["whiskey"], "Test Whiskey")
+        self.assertEqual(json_response["whiskey_type"], {
+                         'id': 7, 'label': 'Single Malt'})
+        self.assertEqual(json_response["country"], "USA")
+        self.assertEqual(json_response["part_of_country"], "Tennessee")
+        self.assertEqual(float(json_response["age_in_years"]), 12.00)
+        self.assertEqual(float(json_response["proof"]), 100.00)
+        self.assertEqual(json_response["color"]["id"], 10)
+        self.assertEqual(json_response["color"]["label"], "amontillado sherry")
+        self.assertEqual(float(json_response["color"]["color_grade"]), 0.9)
+        self.assertEqual(json_response["color"]["hex_code"], "F7C23B")
+        self.assertEqual(json_response["color"]
+                         ["tailwind_name"], "amontillado-sherry")
+        self.assertEqual(json_response["mash_bill"], "100% malted barley")
+        self.assertEqual(
+            json_response["maturation_details"], "former Tennessee whiskey barrels")
+        self.assertEqual(json_response["nose"],
+                         "roasted grain, caramel, vanilla")
+        self.assertEqual(json_response["palate"],
+                         "apple pie, vanilla icecream")
+        self.assertEqual(json_response["finish"], "long and sweet")
+        self.assertEqual(json_response["rating"], {
+                         'id': 5, 'number_rating': 4, 'label': 'very good'})
+        self.assertEqual(json_response["notes"], "tasty whiskey")
